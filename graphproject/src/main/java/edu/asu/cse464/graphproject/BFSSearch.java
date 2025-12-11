87e45f1be2e64178c3a4d4a3c15d84a5b9284984
Abdalla Osman <99369800+AOsman29@users.noreply.github.com>
2025-12-10 21:25:20 -0700

    Refactor: Implemented BFSSearch and DFSSearch as concrete subclasses of SearchTemplate.

--- graphproject/src/main/java/edu/asu/cse464/graphproject/BFSSearch.java
+++ graphproject/src/main/java/edu/asu/cse464/graphproject/BFSSearch.java
@@ -54,3 +54,4 @@ public class BFSSearch extends SearchTemplate {
         return new Path(path);
     }
 }
+//com
--- graphproject/src/main/java/edu/asu/cse464/graphproject/DFSSearch.java
+++ graphproject/src/main/java/edu/asu/cse464/graphproject/DFSSearch.java
@@ -54,3 +54,4 @@ public class DFSSearch extends SearchTemplate {
         return new Path(path);
     }
 }
+//com
